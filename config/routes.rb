466bf8e1539e42d5c60887125d22f720ee962179
Cecile Varin <varincecile@hotmail.com>
2017-11-21 16:17:04 +0100

    todo list ok

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,6 @@ Rails.application.routes.draw do
 
   patch '/tasks/:id', to: 'tasks#update'
 
-  delete 'tasks/:id', to: 'tasks#destroy', as: 'delete'
+  delete 'tasks/:id', to: 'tasks#destroy'
 
 end
